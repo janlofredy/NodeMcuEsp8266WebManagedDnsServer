ebda7bdf35ea0d482e163439ba4911191e48ce0b
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2025-07-23 13:14:08 +0800

    configurable auth

--- DNSManager.ino
+++ DNSManager.ino
@@ -9,18 +9,16 @@ DNSServer dnsServer;
 ESP8266WebServer server(80);
 StaticJsonDocument<1024> dnsDoc;
 StaticJsonDocument<256> netDoc;
-
-const char* www_username = "admin";
-const char* www_password = "esp8266";
+StaticJsonDocument<256> authDoc;
 
 IPAddress local_IP, gateway, subnet;
+String www_username, www_password;
 
 void loadIPConfig() {
   File file = SPIFFS.open("/netconfig.json", "r");
   if (!file) return;
-  DeserializationError error = deserializeJson(netDoc, file);
+  deserializeJson(netDoc, file);
   file.close();
-  if (error) return;
   local_IP.fromString(netDoc["ip"]);
   gateway.fromString(netDoc["gateway"]);
   subnet.fromString(netDoc["subnet"]);
@@ -35,6 +33,25 @@ void saveIPConfig(String ip, String gw, String sn) {
   file.close();
 }
 
+void loadAuth() {
+  File file = SPIFFS.open("/auth.json", "r");
+  if (!file) return;
+  deserializeJson(authDoc, file);
+  file.close();
+  www_username = authDoc["user"].as<String>();
+  www_password = authDoc["pass"].as<String>();
+}
+
+void saveAuth(String user, String pass) {
+  authDoc["user"] = user;
+  authDoc["pass"] = pass;
+  File file = SPIFFS.open("/auth.json", "w");
+  serializeJson(authDoc, file);
+  file.close();
+  www_username = user;
+  www_password = pass;
+}
+
 void saveDNS() {
   File file = SPIFFS.open("/dns.json", "w");
   serializeJson(dnsDoc, file);
@@ -44,7 +61,7 @@ void saveDNS() {
 void loadDNS() {
   File file = SPIFFS.open("/dns.json", "r");
   if (!file) return;
-  DeserializationError error = deserializeJson(dnsDoc, file);
+  deserializeJson(dnsDoc, file);
   file.close();
 }
 
@@ -62,6 +79,7 @@ void setup() {
   SPIFFS.begin();
   loadIPConfig();
   loadDNS();
+  loadAuth();
 
   WiFi.mode(WIFI_STA);
   WiFi.config(local_IP, gateway, subnet);
@@ -71,37 +89,43 @@ void setup() {
   dnsServer.start(DNS_PORT, "*", local_IP);
 
   server.on("/", HTTP_GET, []() {
-    if (!server.authenticate(www_username, www_password)) return server.requestAuthentication();
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
     File file = SPIFFS.open("/index.html", "r");
     server.streamFile(file, "text/html");
     file.close();
   });
 
   server.on("/add", HTTP_POST, []() {
-    if (!server.authenticate(www_username, www_password)) return server.requestAuthentication();
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
     dnsDoc[server.arg("domain")] = server.arg("ip");
     saveDNS();
     server.send(200, "text/plain", "Added");
   });
 
   server.on("/delete", HTTP_POST, []() {
-    if (!server.authenticate(www_username, www_password)) return server.requestAuthentication();
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
     dnsDoc.remove(server.arg("domain"));
     saveDNS();
     server.send(200, "text/plain", "Deleted");
   });
 
   server.on("/list", HTTP_GET, []() {
-    if (!server.authenticate(www_username, www_password)) return server.requestAuthentication();
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
     String output;
     serializeJsonPretty(dnsDoc, output);
     server.send(200, "application/json", output);
   });
 
   server.on("/setip", HTTP_POST, []() {
-    if (!server.authenticate(www_username, www_password)) return server.requestAuthentication();
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
     saveIPConfig(server.arg("ip"), server.arg("gateway"), server.arg("subnet"));
     server.send(200, "text/plain", "IP settings saved. Reboot to apply.");
+  });
+
+  server.on("/setauth", HTTP_POST, []() {
+    if (!server.authenticate(www_username.c_str(), www_password.c_str())) return server.requestAuthentication();
+    saveAuth(server.arg("user"), server.arg("pass"));
+    server.send(200, "text/plain", "Credentials updated. Use new login next time.");
   });
 
   server.begin();
